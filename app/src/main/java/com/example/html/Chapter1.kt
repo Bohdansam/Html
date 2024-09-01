7dfbf3a44ad370ba59ed272cc3f3b623561dab1b
Bohdansam <bogdanbisnes3@gmail.com>
2024-09-01 10:09:26 +0200

    commit1

--- app/src/main/java/com/example/html/Chapter1.kt
+++ app/src/main/java/com/example/html/Chapter1.kt
@@ -1,16 +1,25 @@
 package com.example.html
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.buildSpannedString
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Chapter1 : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_chapter1)
+
 
     }
 }
